b1fd9049a66c2406e83ea6299f976d38b39747b5
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2015-08-29 21:10:25 -0500

    Need to fix concurrent access on the myFuturesThread

--- src/main/java/edu/rice/seclab/dso/Ffastrings.java
+++ src/main/java/edu/rice/seclab/dso/Ffastrings.java
@@ -1,13 +1,18 @@
 package edu.rice.seclab.dso;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -101,12 +106,78 @@ public class Ffastrings {
 	private String myMemDump;
 
 	ExecutorService myExecutor = null;
-
-	ArrayList<Future<?>> myThreadFutures = new ArrayList<Future<?>>();
+	
+	
+	HashMap<String, ArrayList<Future<?>>> myThreadFutures = new HashMap<String, ArrayList<Future<?>>>();
 	private boolean myLiveUpdate = false;
 	private int myNumExecutors;
 	private Long myMinStrLen;
-
+	Writer OutputFileWithLength = null;
+	Writer BasicOutputFile = null;
+	
+	void closeDownWriters() {
+		if (OutputFileWithLength != null) {
+			try {
+				OutputFileWithLength.flush(); 
+				OutputFileWithLength.close();
+			} catch (Exception ex) {/*ignore*/}
+		}
+		
+		if (BasicOutputFile != null) {
+			try {
+				BasicOutputFile.flush(); 
+				BasicOutputFile.close();
+			} catch (Exception ex) {/*ignore*/}
+		}
+	}
+	
+	private void setOutputFileWithLength(File file) {
+		try {
+			OutputFileWithLength = new BufferedWriter(new OutputStreamWriter(
+		          new FileOutputStream(file), "utf-8"));
+		    //writer.write(greppableOutput);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+		   //try {writer.close();} catch (Exception ex) {/*ignore*/}
+		}
+	}
+		
+	
+	private void writeBasicOutput(String output) {
+		if (BasicOutputFile!=null) {
+			try {
+				BasicOutputFile.write(output);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void writeOutputWithLength(String output) {
+		if (OutputFileWithLength!=null) {
+			try {
+				OutputFileWithLength.write(output);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void setBasicOutputFile(File file) {
+		try {
+		    BasicOutputFile = new BufferedWriter(new OutputStreamWriter(
+		          new FileOutputStream(file), "utf-8"));
+		    //writer.write(greppableOutput);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+		   //try {writer.close();} catch (Exception ex) {/*ignore*/}
+		}		
+	}
+	
 	public Ffastrings(String memory_dump_file, Long offset,
 			Integer numThreads, boolean liveUpdate, Long minStrLen) {
 		myNumExecutors = numThreads * (Utils.allStringTypes().size());
@@ -157,9 +228,49 @@ public class Ffastrings {
 			}
 		}
 	}
+	
+	void performMergeOnFileKey (String filename_key) {
+		
+		HashMap<Long, StringInfo> si_hm = myStringInfoMap.get(filename_key);
+		ArrayList<Long> locations = new ArrayList<Long>();
+		locations.addAll(si_hm.keySet());
+		Collections.sort(locations);
+		for (int i = 0; i < locations.size(); i++) {
+			Long offset = locations.get(i);
+			StringInfo si = si_hm.containsKey(offset) ? si_hm.get(offset)
+					: null;
+
+			if (si == null)
+				continue; // chances are it was removed and merged
+			StringInfo last_si = null, next_si = si_hm.get(offset + si.length());
+			while (si_hm.containsKey(offset + si.length())  && (last_si != next_si)) {
+				
+				next_si = si_hm.get(offset + si.length());
+				if (si.getStringType() != next_si.getStringType())
+					break;
+				//System.out.println(String.format("%s @ %08x and %s @%08x overlap", si, si.getLocation(), next_si, next_si.getLocation()));
+				byte[] newValue = new byte[(int) si.length()
+						+ (int) next_si.length()];
+				System.arraycopy(si.getBytes(), 0, newValue, 0,
+						(int) si.length());
+				System.arraycopy(next_si.getBytes(), 0, newValue,
+						(int) si.length(), (int) next_si.length());
+				try {
+					si = new StringInfo(si.getFilename(), offset, newValue,
+							si.getStringType());
+					si_hm.put(offset, si);
+					si_hm.remove(next_si.getLocation());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				last_si = next_si;
+			}
+		}
+
+	}
 
 	void executeFileScans() {
-		// System.out.println("Starting the binary string search");
+		System.out.println("Starting the binary string search");
 		for (File file : myTargetFiles) {
 			try {
 				performFileScan(file);
@@ -168,14 +279,34 @@ public class Ffastrings {
 				e.printStackTrace();
 			}
 		}
-
-		while (!myThreadFutures.isEmpty()) {
-			Future<?> p = myThreadFutures.get(0);
-			if (p.isDone()) {
-				myThreadFutures.remove(0);
-			} else {
-				// System.out.println(String.format("Waiting on %d threads to complete.",
-				// myThreadFutures.size()));
+		boolean exit = myThreadFutures.isEmpty();
+		while (!exit) {
+			Set<String> filename_keys = myThreadFutures.keySet(); 
+			for (String filename_key : filename_keys) {
+				ArrayList<Future<?>> value = myThreadFutures.get(filename_key);
+				int idx = 0;
+				ArrayList<Future<?>> new_value = new ArrayList<Future<?>>(); 
+				for (Future<?> p : value) {
+					if (!p.isDone()) new_value.add(p);
+				}
+				
+				if (new_value.size() == 0) {
+					synchronized (myThreadFutures) {
+						myThreadFutures.remove(filename_key);
+						// dump strings to file
+						// perform string merge						
+					}
+					performMergeOnFileKey(filename_key);
+					performDumpOnFileKey(filename_key);
+				} else if (value.size() != new_value.size()){
+					synchronized (myThreadFutures) {
+						myThreadFutures.put(filename_key, new_value);
+					}					
+					System.out.println(String.format("Waiting on %d threads to complete for %s",
+							   new_value.size(), filename_key));
+				}
+			}
+			if (myThreadFutures.size() == 1) {	
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -183,47 +314,13 @@ public class Ffastrings {
 					e.printStackTrace();
 				}
 			}
+			exit = myThreadFutures.isEmpty();
 		}
 		myExecutor.shutdown();
 		while (!myExecutor.isTerminated()) {
 		}
+		closeDownWriters();
 
-		// perform string merge
-		for (HashMap<Long, StringInfo> si_hm : myStringInfoMap.values()) {
-			ArrayList<Long> locations = new ArrayList<Long>();
-			locations.addAll(si_hm.keySet());
-			Collections.sort(locations);
-			for (int i = 0; i < locations.size(); i++) {
-				Long offset = locations.get(i);
-				StringInfo si = si_hm.containsKey(offset) ? si_hm.get(offset)
-						: null;
-
-				if (si == null)
-					continue; // chances are it was removed and merged
-				while (si_hm.containsKey(offset + si.length())) {
-					
-					StringInfo next_si = si_hm.get(offset + si.length());
-					if (si.getStringType() != next_si.getStringType())
-						break;
-					//System.out.println(String.format("%s @ %08x and %s @%08x overlap", si, si.getLocation(), next_si, next_si.getLocation()));
-					byte[] newValue = new byte[(int) si.length()
-							+ (int) next_si.length()];
-					System.arraycopy(si.getBytes(), 0, newValue, 0,
-							(int) si.length());
-					System.arraycopy(next_si.getBytes(), 0, newValue,
-							(int) si.length(), (int) next_si.length());
-					try {
-						si = new StringInfo(si.getFilename(), offset, newValue,
-								si.getStringType());
-						si_hm.put(offset, si);
-						si_hm.remove(next_si.getLocation());
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-
-		}
 
 		int totalHits = 0, uniqueKeys = 0;
 		HashSet<String> containedFiles = new HashSet<String>();
@@ -245,6 +342,13 @@ public class Ffastrings {
 
 	}
 
+	private void performDumpOnFileKey(String filename_key) {
+		String output = getBasicOutput(filename_key);
+		writeBasicOutput(output);
+		output = getOutputWithLength(filename_key);
+		writeOutputWithLength(output);
+	}
+
 	public String getGreppableOutput() {
 		ArrayList<String> output = new ArrayList<String>();
 		for (HashMap<Long, StringInfo> si_hm : myStringInfoMap.values()) {
@@ -260,28 +364,41 @@ public class Ffastrings {
 	}
 
 	public String getBasicOutput() {
+		StringBuffer sb = new StringBuffer();
+		for (String key : myStringInfoMap.keySet()) {
+			sb.append(getBasicOutput(key));
+		}
+		return sb.toString();
+	}
+	
+	public String getBasicOutput(String key) {
+		HashMap<Long, StringInfo> si_hm = myStringInfoMap.get(key);
 		ArrayList<String> output = new ArrayList<String>();
-		for (HashMap<Long, StringInfo> si_hm : myStringInfoMap.values()) {
-			for (StringInfo bsi : si_hm.values()) {
-
-				String s = bsi.toBasicOutput();
-				if (s.length() > 0)
-					output.add(s);
-			}
+		for (StringInfo bsi : si_hm.values()) {
+			String s = bsi.toBasicOutput();
+			if (s.length() > 0)
+				output.add(s);
 		}
 		Collections.sort(output);
 		return StringUtils.join(output, "\n") + "\n";
 	}
 	
 	public String getOutputWithLength() {
+		StringBuffer sb = new StringBuffer();
+		for (String key : myStringInfoMap.keySet()) {
+			sb.append(getOutputWithLength(key));
+		}
+		return sb.toString();
+	}
+	
+	public String getOutputWithLength(String key) {
+		HashMap<Long, StringInfo> si_hm = myStringInfoMap.get(key);
 		ArrayList<String> output = new ArrayList<String>();
-		for (HashMap<Long, StringInfo> si_hm : myStringInfoMap.values()) {
-			for (StringInfo bsi : si_hm.values()) {
+		for (StringInfo bsi : si_hm.values()) {
 
-				String s = bsi.toOutputStringWithLength();
-				if (s.length() > 0)
-					output.add(s);
-			}
+			String s = bsi.toOutputStringWithLength();
+			if (s.length() > 0)
+				output.add(s);
 		}
 		Collections.sort(output);
 		return StringUtils.join(output, "\n") + "\n";
@@ -315,9 +432,9 @@ public class Ffastrings {
 		long fileSize = file.length();
 		long chunkSz = fileSize / myNumThreads;
 		RandomAccessFile fhandle = new RandomAccessFile(file, "r");
-
+		String key  = file.getAbsolutePath();
 		for (long offset = myStartOffset; offset < fileSize; offset += chunkSz) {
-			chunkSz = offset + chunkSz > fileSize ? fileSize - offset : chunkSz;
+			chunkSz = offset + chunkSz >= fileSize ? fileSize - offset : chunkSz;
 			byte[] res = null;
 			try {
 				fhandle.seek(offset);
@@ -340,7 +457,12 @@ public class Ffastrings {
 						myStringInfoMap.get(file.getAbsolutePath()), stype,
 						myLiveUpdate);
 				Future<?> p = myExecutor.submit(cp);
-				myThreadFutures.add(p);
+				
+				if (!myThreadFutures.containsKey(key))
+					myThreadFutures.put(key, new ArrayList<Future<?>>());
+				
+				ArrayList<Future<?>> value = myThreadFutures.get(key);
+				value.add(p);
 			}
 		}
 	}
@@ -392,21 +514,22 @@ public class Ffastrings {
 		fbs = new Ffastrings(memory_dump_file, offset, numThreads,
 				liveUpdate, minStrLen);
 
+
 		if (fbs != null) {
-			fbs.executeFileScans();
 			if (cli.hasOption(BASIC_OUTPUT)) {
-				File f = new File(cli.getOptionValue(BASIC_OUTPUT));
-				Utils.writeOutputFile(f, fbs.getBasicOutput());
+				fbs.setBasicOutputFile(new File(cli.getOptionValue(BASIC_OUTPUT)));
 			}
 			if (cli.hasOption(OUTPUT_WITH_BYTE_LENGTH)) {
-				File f = new File(cli.getOptionValue(OUTPUT_WITH_BYTE_LENGTH));
-				Utils.writeOutputFile(f, fbs.getOutputWithLength());
+				fbs.setOutputFileWithLength(new File(cli.getOptionValue(OUTPUT_WITH_BYTE_LENGTH)));
 			}
+			fbs.executeFileScans();
 //			if (cli.hasOption(KEY_COUNT_OUTPUT)) {
 //				File f = new File(cli.getOptionValue(KEY_COUNT_OUTPUT));
 //				Utils.writeOutputFile(f, fbs.getByCountsOutput());
 //			}
 		}
 	}
+
+
 
 }
